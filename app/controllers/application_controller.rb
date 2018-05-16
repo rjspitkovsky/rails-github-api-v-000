f3f876c846af307606b2095b6e9e0db22e04cd0a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-16 19:17:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
     def authenticate_user
       # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
       # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
+      client_id = ENV['CLIENT_ID']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
+      redirect_to github_url unless logged_in?
+
     end
 
     def logged_in?
