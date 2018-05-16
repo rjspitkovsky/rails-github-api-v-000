10eba0a180cad20b63339f5dbf2e250dc8d17103
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-16 19:29:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
+
+  
   def create
     resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"], code: params[:code]}, {'Accept' => 'application/json'}
     body_hash = JSON.parse(resp.body)
